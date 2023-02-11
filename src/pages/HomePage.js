bbabc2160f932ed2157d8df9e0197fa4bc0b08d9
Sean Tan <seantan1218@gmail.com>
2023-02-11 14:21:56 +0800

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import HeroBanner from "../components/layout/HomePage/HeroBanner";
+
+const HomePage = () => {
+  return (
+    <div className="mx-3 md:mx-6 my-20">
+      <HeroBanner />
+    </div>
+  );
+};
+
+export default HomePage;
