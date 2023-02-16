e5840818a0f915c577ab656134c031e6018b1ba7
Sean Tan <seantan1218@gmail.com>
2023-02-16 16:29:32 +0800

    Added word to cta

--- src/components/layout/HomePage/Cta.jsx
+++ src/components/layout/HomePage/Cta.jsx
@@ -6,7 +6,7 @@ const Cta = () => {
       <div className=" bg-highlight-eggshell p-8 md:p-20 rounded-md border-2 border-dotted border-text-main">
         <h3 className="text-3xl md:text-5xl font-black leading-none mb-4 text-text-main">
           It's time to stop working in your business, and{" "}
-          <u>working on your business instead.</u>
+          <u>start working on your business instead.</u>
         </h3>
         <p className="text-xl md:text-2xl text-text-main mb-8">
           As a successful founder, you have likely experienced the challenges of
